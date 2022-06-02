b8edc1e526badfde76a5d8ac9d933fcc47e4efcd
i-naeem <inaeem@protonmail.com>
2022-06-02 13:42:18 +0500

    :tada: Setup project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  return res.json({
+    message: 'Home'
+  })
+})
 
 module.exports = router;

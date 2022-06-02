3be707d5f263dc4ae9e50b39e5bd8e7f9e4c5e4f
i-naeem <inaeem@protonmail.com>
2022-06-02 14:47:20 +0500

    :sparkles: add usage

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,27 @@ const isValidUrl = require('../utils/isValidUrl');
 const router = express.Router();
 
 
-router.get('/', async (req, res, next) => {
+router.get('/api', async (req, res, next) => {
+
+  if (req.query.url === undefined) {
+    return res.json({
+      "name": "Cookey",
+      "description": "A rest api to fetch Javascript generated cookies from websites.",
+      "router": {
+        "/api": {
+          "method": "GET",
+          "args": {
+            "url": {
+              "type": "string",
+              "required": true,
+              "description": "Website url you want to get cookies for.",
+            }
+          }
+        }
+      }
+    })
+  }
+
 
   if (!isValidUrl(req.query.url)) {
 

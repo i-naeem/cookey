35bc1db8b74824937e382703a46f6e8550817185
i-naeem <inaeem@protonmail.com>
2022-06-12 12:02:22 +0500

    :bug: remove validations

--- routes/index.js
+++ routes/index.js
@@ -26,15 +26,6 @@ router.get('/api', async (req, res, next) => {
     })
   }
 
-
-  if (!isValidUrl(req.query.url)) {
-
-    return res.status(422).json({
-      status: "FAILED",
-      message: "Invalid Url"
-    })
-  }
-
   try {
     const response = await Cookey.getCookies(req.query.url);
     return res.json(response);
